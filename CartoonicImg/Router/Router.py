f22a0f2f2bee3957edbad5d0c977b90e5059b560
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 13:58:52 +0400

    Router.py
    update

--- CartoonicImg/Router/Router.py
+++ CartoonicImg/Router/Router.py
@@ -4,27 +4,27 @@ Created on Sat Aug 12 20:29:24 2023
 
 @author: farha
 """
+# Importing required modules
 from fastapi import FastAPI
 from fastapi import UploadFile, File
-from controller.contoller import controller
+from controller.contoller import controller  # Importing the 'controller' instance from 'controller' module
 
+# Creating a class called 'Router'
 class Router:
-    app=FastAPI()
+    # Creating an instance of 'FastAPI'
+    app = FastAPI()
     
-    
+    # Defining a route for the root URL '/'
     @app.get('/')
     def index():
-        return {'message':'Welcome to Conversion Image into Cartoon App'}
-        # return view.html
+        return {'message': 'Welcome to Conversion Image into Cartoon App'}
     
+    # Defining a route for the '/cartoonic' endpoint using HTTP POST method
     @app.post('/cartoonic')
-    def Cartoon(file: UploadFile= File(...)):
-        # print(file.filename)
+    def Cartoon(file: UploadFile = File(...)):
+        # Calling the 'cartoonize_image' method from the 'controller' instance
         controller.cartoonize_image(file)
         return {"message": 'Image is Converted, Check Your Directory'}
-        
-    
-        
-        
-        
-router=Router()
+
+# Creating an instance of the 'Router' class
+router = Router()
