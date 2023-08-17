10cf39435fbe7b64d99bdf7e54722c9aa531e233
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 16:05:11 +0400

    main.py

--- OpenClosed/main.py
+++ OpenClosed/main.py
@@ -1,40 +1,13 @@
-
+# Import the 'route' instance from the Routes module
 from routes.Routes import route
+
+# Import the uvicorn library for running the FastAPI app
 import uvicorn
 
-
-
-if __name__=='__main__':
-    uvicorn.run(route.app,host="127.0.0.1",port=8000)
-
-
+# Entry point of the script
+if __name__ == '__main__':
+    # Run the FastAPI app using uvicorn
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Specify the FastAPI app instance (route.app) to run
+    # Bind the app to the localhost (127.0.0.1) on port 8000
+    uvicorn.run(route.app, host="127.0.0.1", port=8000)
