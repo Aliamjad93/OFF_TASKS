2601c6633b8cbd889a79f23e56eca1ff3cb83513
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 14:36:53 +0400

    main.py

--- poetry/backend/app/main.py
+++ poetry/backend/app/main.py
@@ -1,9 +1,9 @@
-# from routes import temp
-from .routes.router import Routes    
-import uvicorn
+# Importing necessary libraries
+from .routes.router import Routes  # Importing the 'Routes' class from 'routes.temp' module
+import uvicorn  # Importing uvicorn for running the FastAPI app
 
-
-
+# Defining a function called 'start'
 def start():
-    
-    uvicorn.run("app.main:Routes.App" ,host="127.0.0.1",port=8000)
+    # Running the FastAPI app using uvicorn
+    # "app.main:Routes.App" specifies the app location and its attribute to run
+    uvicorn.run("app.main:Routes.App", host="127.0.0.1", port=8000)
