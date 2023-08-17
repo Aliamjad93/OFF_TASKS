1b55d6c7e7042691e3b4b799f6ae4f3e56c64245
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 15:52:13 +0400

    router.py

--- MovieRecommender/Router/router.py
+++ MovieRecommender/Router/router.py
@@ -1,23 +1,28 @@
+# Import the FastAPI framework
 from fastapi import FastAPI
+
+# Import necessary classes from your project
 from model.Model import Model
 from Controller.controller import Controller
-class route:
-    app=FastAPI()
-    
 
-    # All Data Route    
+# Create a class for defining routes
+class route:
+    # Initialize the FastAPI app
+    app = FastAPI()
+
+    # Define a route for retrieving all data
     @app.get('/alldata')
     def AllData():
+        # Call the Alldata() function from the Controller class to fetch all data
         return Controller.Alldata()
-        
-    # Prediction Route
-    @app.post('/predict')
-    def predict(name:Model):
-        
-        return Controller.Predict(name.movie_name)
-        
-    
-    
-    
-router=route()
 
+    # Define a route for making predictions
+    @app.post('/predict')
+    def predict(name: Model):
+        # Extract the movie name from the input Model instance
+        movie_name = name.movie_name
+        # Call the Predict() function from the Controller class to make predictions
+        return Controller.Predict(movie_name)
+
+# Create an instance of the 'route' class to initialize routes
+router = route()
