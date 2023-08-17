5f982252e5b3811cece8becbe7e3a58a59e12ed7
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 16:04:14 +0400

    Routes.py

--- OpenClosed/routes/Routes.py
+++ OpenClosed/routes/Routes.py
@@ -1,33 +1,25 @@
-
-#------ FastAPI Modules
-
+# Import necessary modules from FastAPI
 from fastapi import FastAPI, UploadFile, File
 
+# Import the controller instance from the Controller module
 from controller.Controller import obj_controller
 
-
-
-
+# Class defining the routes for FastAPI
 class Routes:
-    app=FastAPI()
+    # Initialize the FastAPI app
+    app = FastAPI()
     
-    
-    
+    # Route for the root endpoint
     @app.get('/')
     def index():
+        # Return a welcome message
         return {'message': 'Welcome to Cat vs Dog Prediction'}
     
+    # Route for image prediction
     @app.post('/predicts')
-    def AddImage(file: UploadFile= File(...)):
-        
+    def AddImage(file: UploadFile = File(...)):
+        # Call the prediction method from the controller and return the result
         return obj_controller.predict(file)
-        
 
-
-        
-    
-
-route=Routes()
-    
-
-
+# Create an instance of the Routes class to define the routes
+route = Routes()
