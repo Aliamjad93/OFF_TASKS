47294cd5850dbe1a45bd1b3b0de1f5a4d194813d
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 14:30:11 +0400

    controller.py
    updated

--- poetry/backend/app/controller/controller.py
+++ poetry/backend/app/controller/controller.py
@@ -1,40 +1,35 @@
-from app.mongo.db import Database
+from app.mongo.db import Database  # Import your database module
 from bson.objectid import ObjectId
 
 class Controller:
     def Main():
-        return ({'record': 'Hello World'})
+        return {'record': 'Hello World'}  # Return a simple dictionary
     
     def Create_Data(Data):
-        Data=dict(Data)
-        Database.insert_one(Data)
+        Data = dict(Data)  # Convert input data to a dictionary
+        Database.insert_one(Data)  # Insert data into the database
+        
         return {"message": 'record added successfully'}
     
     def Get_Data():
-        Response=Database.find()
-        Records_List=[]
+        Response = Database.find()  # Retrieve data from the database
         
-        for j in Response:
-            Records_List.append(j)
+        Records_List = []
+        for record in Response:   # applying loop to fetch data from Objects by ObjectId
+            
+            record_dict = dict(record) #converting data into dictionary for returning record into json
+            if "_id" in record_dict:
+                record_dict["_id"] = str(record_dict["_id"])  # Convert ObjectId to string
+            Records_List.append(record_dict)  # Append processed record to the list
         
         return Records_List
     
-    def Data():
-        DataFrame=Controller.Get_Data()
-        
-        return {'msg':f'{DataFrame}'} 
-    
     def Get_Specific_Data(data):
-        Target_Id = ObjectId(data)
+        Target_Id = ObjectId(data)  # Convert input data to ObjectId
     
-        Record=Database.find_one({'_id':Target_Id})
+        Record = Database.find_one({'_id': Target_Id})  # Retrieve a specific record
     
-        return str(Record)  
+        return str(Record)  # Return the retrieved record as a string
     
-
-Contoller=Controller()
-
-
-
-
-
+# Create an instance of the Controller class
+Controller_Instance = Controller()
