16df073bffae3113f9c4e573517982876058208b
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 15:54:20 +0400

    main.py

--- MovieRecommender/main.py
+++ MovieRecommender/main.py
@@ -1,6 +1,13 @@
+# Import the 'router' instance from the Router module
 from Router.router import router
+
+# Import the 'uvicorn' library for running the FastAPI app
 import uvicorn
-if __name__=="__main__":
-    uvicorn.run(router.app,host="127.0.0.1" , port=8000)
-    
-    
+
+# Entry point of the script
+if __name__ == "__main__":
+    # Run the FastAPI app using uvicorn
+
+    # Specify the FastAPI app instance (router.app) to run
+    # Bind the app to the localhost (127.0.0.1) on port 8000
+    uvicorn.run(router.app, host="127.0.0.1", port=8000)
