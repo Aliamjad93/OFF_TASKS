81491568f6140f2cb9a3185294cb218458cc2212
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 14:30:42 +0400

    models.py

--- poetry/backend/app/model/models.py
+++ poetry/backend/app/model/models.py
@@ -1,6 +1,9 @@
+# Importing the necessary library
 from pydantic import BaseModel
 
+# Creating a Pydantic BaseModel called 'Student'
 class Student(BaseModel):
-    name: str
-    description: str
-    age: int
+    # Defining attributes with their types for data validation and serialization
+    name: str  # Attribute for the student's name (type: str)
+    description: str  # Attribute for the student's description (type: str)
+    age: int  # Attribute for the student's age (type: int)
