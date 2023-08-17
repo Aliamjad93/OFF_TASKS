e80c92543820e0b804dcf8023d0deee24bdabb9f
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-17 15:53:29 +0400

    dataloader.py

--- MovieRecommender/db/dataloader.py
+++ MovieRecommender/db/dataloader.py
@@ -1,9 +1,16 @@
+# Import the pandas library and alias it as 'pd'
 import pandas as pd
 
+# Define a class for loading data
 class DataLoader:
     def __init__(self):
+        # Initialize the class by reading two CSV files into DataFrames
+
+        # Load movie data from a CSV file into a 'movies' DataFrame
         self.movies = pd.read_csv(r'C:\Users\farha\.spyder-py3\Task1\MovieRecommender\db\tmdb_5000_movies.csv')
-        self.credits = pd.read_csv(r'C:\Users\farha\.spyder-py3\Task1\MovieRecommender\db\tmdb_5000_credits.csv')
         
-dataLoader=DataLoader()
-         
+        # Load credits data from a CSV file into a 'credits' DataFrame
+        self.credits = pd.read_csv(r'C:\Users\farha\.spyder-py3\Task1\MovieRecommender\db\tmdb_5000_credits.csv')
+
+# Create an instance of the 'DataLoader' class to load data into DataFrames
+dataLoader = DataLoader()
