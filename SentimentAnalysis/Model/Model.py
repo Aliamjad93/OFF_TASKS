f289975a33e02b791301d3fca8abc7601946ef92
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-21 17:37:10 +0400

    Model.py

--- SentimentAnalysis/Model/Model.py
+++ SentimentAnalysis/Model/Model.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
 
+# Define a Pydantic model class to represent input data
 class Model(BaseModel):
+    # Define a field named 'text' with type annotation 'str'
     text: str
