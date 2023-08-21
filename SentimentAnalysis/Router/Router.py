c9feea496a93d297794ac37f1bff4b0063268101
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-21 17:34:54 +0400

    Router.py

--- SentimentAnalysis/Router/Router.py
+++ SentimentAnalysis/Router/Router.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from Controller.Controller import controller
 from Model.Model import Model 
+
+# Create an instance of FastAPI
 class Router:
-    app=FastAPI()
-    
+    app = FastAPI()
+
+    # Define a route for the root URL
     @app.get('/')
     def index():
         return "Welcome to Text Nature Level Prediction"
-    
-    
+
+    # Define a route for handling POST requests
     @app.post('/')
     def prediction(text: Model):
+        # Call the MakePrediction method from the controller
         return controller.MakePrediction(text.text)
 
-
-router=Router()        
+# Create an instance of the Router class
+router = Router()
