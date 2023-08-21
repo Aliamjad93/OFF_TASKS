1b2a06e2cdf2d24f3d31f10e4938a24d870191dc
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-21 17:36:12 +0400

    Controller.py

--- SentimentAnalysis/Controller/Controller.py
+++ SentimentAnalysis/Controller/Controller.py
@@ -1,24 +1,26 @@
 import joblib
 from dotenv import load_dotenv
 import os
-from .SVM_Model.SVM_Model import vectorizer,clean_text
+from .SVM_Model.SVM_Model import vectorizer, clean_text
 
 class Controller:
-    
     def __init__(self):
+        # Load environment variables from the specified file
         load_dotenv(dotenv_path=r"C:\Users\farha\.spyder-py3\Task1\SentimentAnalysis\Controller\src.env")
-        model=os.getenv("model")
-        self.model=joblib.load(open(model,'rb'))
+        # Get the model file path from environment variables
+        model = os.getenv("model")
+        # Load the pre-trained model using joblib
+        self.model = joblib.load(open(model, 'rb'))
         
+    def MakePrediction(self, text):
+        # Clean the input text
+        text = clean_text(text)
         
+        # Transform the cleaned text into a vector using the vectorizer
+        pred_vectors = vectorizer.transform([text])
         
-        
-    def MakePrediction(self,text):
-        text=text
-        text=clean_text(text)
-        
-        pred_vectors=vectorizer.transform([text])
+        # Perform prediction using the loaded model and return the result
         return self.model.predict(pred_vectors)[0]
-        
-    
-controller=Controller()
+
+# Create an instance of the Controller class
+controller = Controller()
