ac91ae2e39be49bae8c03c4618bffd42d200dbbb
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-21 17:38:41 +0400

    main.py

--- SentimentAnalysis/main.py
+++ SentimentAnalysis/main.py
@@ -1,10 +1,21 @@
+# Import the router instance from the Router module
 from Router.Router import router
+
+# Import uvicorn for running the FastAPI app
 import uvicorn
+
+# Import the load_dotenv function and os module
 from dotenv import load_dotenv
 import os
 
-if __name__=="__main__":
+# Check if this script is being executed as the main program
+if __name__ == "__main__":
+    # Load environment variables from the specified file
     load_dotenv(dotenv_path=r"C:\Users\farha\.spyder-py3\Task1\SentimentAnalysis\Controller\src.env")
-    port=os.get("port")
-    host=os.get("host")
-    uvicorn.run(router.app,host=host,port=port)
+    
+    # Get the port and host from environment variables
+    port = (os.getenv("port"))  
+    host = os.getenv("host")
+    
+    # Run the FastAPI app using uvicorn
+    uvicorn.run(router.app, host=host, port=port)
