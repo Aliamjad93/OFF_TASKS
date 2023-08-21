5e49c75a7c41f4c92757ae15fc4ef3e8aedd07b6
Ali Amjad <60213582+Aliamjad93@users.noreply.github.com>
2023-08-21 10:21:20 +0400

    main.py

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -1,5 +1,16 @@
 from Routes import Routes
 import uvicorn
+# Importing the 'load_dotenv' function from the 'dotenv' library
+from dotenv import load_dotenv
+
+# Importing the 'os' module for working with environment variables
+import os
 
 if __name__=="__main__":
-    uvicorn.run(Routes.route.app,host="127.0.0.1",port=8000)    
+    # Load environment variables from .env file
+    load_dotenv(dotenv_path="C:/Users/farha/.spyder-py3/Task1/ChatBot/src.env")
+    
+    # Access the 'port' environment variable
+    host = os.getenv("host")
+    port = os.getenv("port")
+    uvicorn.run(Routes.route.app,host=host,port=port)    
